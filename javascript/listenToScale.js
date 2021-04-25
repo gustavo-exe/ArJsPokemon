4570af21985735cd8d2ad10ae50b1c7fcb4e06d0
gustavo-exe <gus.elim2@gmail.com>
2021-04-24 22:55:15 -0600

    UI para aumentar escala con asset de la gorra

---
+++ javascript/listenToScale.js
@@ -0,0 +1,24 @@
+AFRAME.registerComponent("listen-to-scale", {
+  init: function () {
+    this.handleClick = this.handleClick.bind(this);
+    this.scaleFactor = 0.15;
+    
+    var up = document.getElementById("up");
+    var down = document.getElementById("down");
+
+    up.addEventListener("click", this.handleClick);
+    down.addEventListener("click", this.handleClick);
+  },
+
+  handleClick: function (e) {
+    if (e.target.id === "up") {
+      this.scaleFactor *= 1.5;
+    } else {
+      this.scaleFactor *= 0.6667;
+    }
+
+    this.el.object3D.scale.x = this.scaleFactor;
+    this.el.object3D.scale.y = this.scaleFactor;
+    this.el.object3D.scale.z = this.scaleFactor;
+  },
+});
